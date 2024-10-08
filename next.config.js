6afcfca1079816d558496f0aaf85fa1398487700
jduns <92645693+jduns@users.noreply.github.com>
2024-10-08 09:24:43 -0400

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,17 @@
 module.exports = {
   reactStrictMode: true,
+  experimental: {
+    esmExternals: 'loose',
+  },
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        ...config.resolve.fallback,
+        fs: false,
+        net: false,
+        tls: false,
+      };
+    }
+    return config;
+  },
 }
