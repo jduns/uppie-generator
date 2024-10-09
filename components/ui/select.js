11c8d6e95993d1fecb4f484c351f345a575bd78d
jduns <92645693+jduns@users.noreply.github.com>
2024-10-09 09:01:38 -0400

    Update select.js

--- components/ui/select.js
+++ components/ui/select.js
@@ -8,7 +8,7 @@ import {
   SelectLabel,
   SelectTrigger,
   SelectValue,
-} from "../components/ui/select";
+} from "./select";
 
 const StoryTypeSelect = ({ storyParams, handleInputChange }) => {
   return (
