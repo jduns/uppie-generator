33e6c38742df537b4ab93512e6f77597fed19d2e
jduns <92645693+jduns@users.noreply.github.com>
2024-10-09 09:00:06 -0400

    Update select.js

--- components/ui/select.js
+++ components/ui/select.js
@@ -1,4 +1,4 @@
-import * as React from "react"
+import * as React from "react";
 
 import {
   Select,
@@ -8,17 +8,24 @@ import {
   SelectLabel,
   SelectTrigger,
   SelectValue,
-} from "@/components/ui/select"
+} from "../components/ui/select";
 
-<Select name="storyType" value={storyParams.storyType} onValueChange={(value) => handleInputChange({ target: { name: 'storyType', value } })}>
-  <SelectTrigger>
-    <SelectValue placeholder="Select story type" />
-  </SelectTrigger>
-  <SelectContent>
-    <SelectItem value="adventure">Adventure</SelectItem>
-    <SelectItem value="fantasy">Fantasy</SelectItem>
-    <SelectItem value="educational">Educational</SelectItem>
-  </SelectContent>
-</Select>
-  )
-}
+const StoryTypeSelect = ({ storyParams, handleInputChange }) => {
+  return (
+    <Select 
+      name="storyType" 
+      value={storyParams.storyType} 
+      onValueChange={(value) => handleInputChange({ target: { name: 'storyType', value } })}>
+      <SelectTrigger>
+        <SelectValue placeholder="Select story type" />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectItem value="adventure">Adventure</SelectItem>
+        <SelectItem value="fantasy">Fantasy</SelectItem>
+        <SelectItem value="educational">Educational</SelectItem>
+      </SelectContent>
+    </Select>
+  );
+};
+
+export default StoryTypeSelect;
