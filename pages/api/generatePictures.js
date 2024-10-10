a544aab2473b279dedcd23b689f57d728f899e8a
jduns <92645693+jduns@users.noreply.github.com>
2024-10-10 13:32:54 -0400

    Update generatePictures.js

--- pages/api/generatePictures.js
+++ pages/api/generatePictures.js
@@ -1,5 +1,5 @@
 // pages/api/generatePictures.js
-import { generateUniqueId } from '../../utils';
+import { generateUniqueId } from '../utils';
 
 export default async function handler(req, res) {
   if (req.method === 'POST') {
