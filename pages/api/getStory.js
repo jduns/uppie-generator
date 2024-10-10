55cfeef87fce38c92fac55e1fbd2fee78a04e1d1
jduns <92645693+jduns@users.noreply.github.com>
2024-10-10 13:24:16 -0400

    Update getStory.js

--- pages/api/getStory.js
+++ pages/api/getStory.js
@@ -1,17 +1,14 @@
-import { createClient } from '@vercel/kv';
-
-const kv = createClient({
-  url: process.env.KV_REST_API_URL,
-  token: process.env.KV_REST_API_TOKEN,
-});
-
+// pages/api/getStory.js
 export default async function handler(req, res) {
   if (req.method === 'GET') {
     const { id } = req.query;
-
     try {
-      const story = await kv.get(`story:${id}`);
-      res.status(200).json({ story });
+      if (typeof window !== 'undefined') {
+        const story = localStorage.getItem(`story:${id}`);
+        res.status(200).json({ story });
+      } else {
+        res.status(200).json({ story: null });
+      }
     } catch (error) {
       console.error('Error fetching story:', error);
       res.status(500).json({ error: 'Internal server error' });
