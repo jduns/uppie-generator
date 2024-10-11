1aa60bf540280d74fa695e2ce08b0a58c99cd28a
jduns <92645693+jduns@users.noreply.github.com>
2024-10-11 11:00:12 -0400

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,6 +1,6 @@
 import { useState, useEffect } from 'react';
 import styles from '../styles/index.module.css';
-import clientPromise from '../../lib/mongodb';
+import clientPromise from '../lib/mongodb';
 
 // API handler (if needed in the same file)
 export const handler = async (req, res) => {
